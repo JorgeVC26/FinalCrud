491f8ff76a3a820e7fd0b87ee6102a119ab6d012
Jorge Villalobos <jorvillacar26@gmail.com>
2026-03-19 17:30:41 -0600

    conf db connection

--- FinalCrud/Data/AppDbContext.cs
+++ FinalCrud/Data/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace FinalCrud.Data
+﻿using Microsoft.EntityFrameworkCore;
+using FinalCrud.Models;
+using System.Collections.Generic;
+
+namespace FinalCrud.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
     }
 }
